a3041beb0ce48fe898720f063f8d6423978a55b8
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-09 14:39:22 +0530

    ...

--- run-endeavour.sh
+++ run-endeavour.sh
@@ -10,17 +10,13 @@ fi
 if [ ! -d "feedback_recordings" ]; then
   mkdir feedback_recordings
 fi
-# If the "speech_tts" folder doesn't exist, create it
-if [ ! -d "speech_tts" ]; then
-  mkdir speech_tts
-fi
-# If the "cache.json" file doesn't exist, create it
-if [ ! -f "cache.json" ]; then
-  echo "{\n\t\"train-data-hash\":\"\"}" > cache.json
-fi
 # If the "Feedback/feedbacks.csv" file doesn't exist, create it
 if [ ! -f "Feedback/feedbacks.csv" ]; then
   echo "id,feedback\n" > Feedback/feedbacks.csv
+fi
+# If the "tts_cache" folder doesn't exist, create it
+if [ ! -d "tts_cache" ]; then
+  mkdir tts_cache
 fi
 
 python3 UIBot/main.py
