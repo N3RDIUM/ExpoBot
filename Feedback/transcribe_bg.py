c5d679cbff25b0c5b5e6b1e0d286be354d705e0f
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-09 15:29:47 +0530

    ...

--- Feedback/transcribe_bg.py
+++ Feedback/transcribe_bg.py
@@ -6,30 +6,33 @@ def update_feedbacks(data):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedbacks.json")) as f:
         current = json.load(f)
         current.update(data)
-        json.dump(current, f)
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedbacks.json"), "w") as f:
+        json.dump(current, f, indent=4)
 
 r = sr.Recognizer()
-langs = [
+languages = [
     "en-US",
     "hi-IN",
-    "bn-IN",
-    "gu-IN",
-    "kn-IN",
-    "ml-IN",
     "mr-IN",
-    "pa-IN",
-    "ta-IN",
+    "kn-IN",
 ]
+def transcribe_all_languages(path):
+    result = {}
+    for language in languages:
+        with sr.AudioFile(path) as source:
+            audio = r.record(source)
+        try:
+            result[language] = r.recognize_google(audio, language=language)
+        except sr.UnknownValueError:
+            result[language] = ""
+        except sr.RequestError:
+            result[language] = ""
+    return result
+
 while True:
     # Remember, any language can be used here
     files = os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedback_recordings"))
     for file in files:
-        data = dict(
-            r.recognize_google(
-                sr.AudioData(
-                    open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedback_recordings", file), "rb").read(),
-                    sample_rate=16000,
-                    sample_width=4
-                )
-        ))
-        print(data)
+        lng = transcribe_all_languages(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedback_recordings", file))
+        update_feedbacks({file.split(".")[0]: lng})
+        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "feedback_recordings", file))
