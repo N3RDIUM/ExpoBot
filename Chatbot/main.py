3066231c0ecb2379696fbfc2a518a77cdbbf77df
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-31 12:58:40 +0530

    Whoops!

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -2,7 +2,7 @@ import logging
 import time
 import fuzzywuzzy
 logging.basicConfig(level=logging.INFO)
-DEV = True
+DEV = False
 # for older hardware, set this to True
 # TODO: Also cache fallbacks
 
